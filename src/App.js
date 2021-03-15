4117f598bc7f975dc914c810a6a62bf5e682aadf
Don <don.dongjun.yu@gmail.com>
2021-03-15 15:37:34 +0900

    add header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div id="header">
-      <div id="header-area"></div>
+    <div>
+      <div id="header">
+        <div id="header-area">
+          <Link to="/">
+            <img src="/images/icons/logo.png" alt="logo" />
+          </Link>
+        </div>
+      </div>
+      <div id="body">body section</div>
     </div>
   );
 }
